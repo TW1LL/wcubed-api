d2414bc98e96b378e6d2d18a88868857be73bc23
Tom Fitzpatrick <tfitz237@gmail.com>
2017-10-15 17:00:28 -0400

    testing deploy

--- src/deploy.ts
+++ src/deploy.ts
@@ -2,8 +2,14 @@ import {Context} from 'koa';
 const exec = require('child_process').exec;
 
 export function deploy(ctx: Context) {
-    const stdout = exec('sh init/deploy.sh');
+    var stdout = exec('git pull', function (error, stdout, stderr) {
+        console.log(stdout);
+        console.log(stderr);
+        if (error) {
+            console.log(error);
+        }
+    });
     ctx.body = {
-        result: 'Deploying...'
-    };
+        result: 'Trying to deploy...'
+    }
 }
