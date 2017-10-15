ad4f3bbdfa9ae68def373966c511e22fb18f03ac
Tom Fitzpatrick <tfitz237@gmail.com>
2017-10-15 14:22:48 -0400

    add some echoing

--- init/postinstall.sh
+++ init/postinstall.sh
@@ -1,4 +1,8 @@
+echo "Copying problem files..."
 cp init/easypost.js node_modules/@easypost/api/easypost.js
 cp init/koa/index.d.ts node_modules/@types/koa/index.d.ts
+echo "Updating models.ts"
 node src/models/index.js
+echo "Compiling Typescript"
 tsc
+echo "Done!"
