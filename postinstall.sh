c43db1943a92f339aaeb521bd7a7a7d61ec3f658
tfitz237 <tfitz237@gmail.com>
2017-10-14 23:25:47 -0400

    Update postinstall.sh

--- postinstall.sh
+++ postinstall.sh
@@ -1,5 +1,5 @@
 sed -i -e "/^interface Request extends BaseRequest {$/a"$;\\\n'"body: any;"'\n' node/modules/@types/koa/index.d.ts
-sed -i "var regeneratorRuntime=require('regenerator-runtime/runtime');" node_modules/@easypost/api/easypost.js
+sed -i "var regeneratorRuntime=require\('regenerator-runtime/runtime'\);" node_modules/@easypost/api/easypost.js
 ## edit @easypost/api/easypost.js
 ##### add 'var regeneratorRuntime=require('regenerator-runtime/runtime');' at the very beginning
 node src/models/index.js
