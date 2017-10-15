b86b27c23573a929deac49538e4545cf9abdd04c
Tom Fitzpatrick <tfitz237@gmail.com>
2017-10-15 18:28:58 -0400

    Add another log on startup

--- src/utils/db.ts
+++ src/utils/db.ts
@@ -3,6 +3,7 @@ export class Db {
     public connection: Connection;
 
     public async connect(dbConfig: ConnectionOptions): Promise <Connection> {
+        console.log('Connecting to database as ' + (dbConfig as any).username);
         this.connection = await createConnection(dbConfig);
         return this.connection;
     }
