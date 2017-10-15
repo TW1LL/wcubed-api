d764708c3e143214f9e8c8080ef50c83440ac17e
Tom Fitzpatrick <tfitz237@gmail.com>
2017-10-15 16:52:55 -0400

    testing deploy

--- src/deploy.ts
+++ src/deploy.ts
@@ -2,7 +2,7 @@ import {Context} from 'koa';
 const exec = require('child_process').execSync;
 
 export function deploy(ctx: Context) {
-    const stdout = exec('sh ../init/deploy.sh').toString();
+    const stdout = exec('sh init/deploy.sh').toString();
     ctx.body = {
         result: stdout
     };
