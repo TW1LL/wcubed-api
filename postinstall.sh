fa86c20ca37ec3d3f717281fb0662bddef3d2416
tfitz237 <tfitz237@gmail.com>
2017-10-14 23:22:45 -0400

    Update postinstall.sh

--- postinstall.sh
+++ postinstall.sh
@@ -1,7 +1,7 @@
 cd wcubed-api
 npm install
-## edit @types/koa/index.d.ts -
-##### add 'body: any;' to 'interface Request extends BaseRequest {' at around line 626
+sed -i -e "/^interface Request extends BaseRequest {$/a"$;\\\n'"body: any;"'\n' node/modules/@types/koa/index.d.ts
+sed -i "var regeneratorRuntime=require('regenerator-runtime/runtime');" node_modules/@easypost/api/easypost.js
 ## edit @easypost/api/easypost.js
 ##### add 'var regeneratorRuntime=require('regenerator-runtime/runtime');' at the very beginning
 node src/models/index.js
