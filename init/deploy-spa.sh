8b423b4e1024e497a803e0d80310b14cd237f869
Will Wagner <willwagner602@gmail.com>
2017-10-21 18:08:51 -0400

    just kidding apparently we didn't need the environment stuff at all

--- init/deploy-spa.sh
+++ init/deploy-spa.sh
@@ -1,5 +1,3 @@
 cd /var/node/wcubed-spa
 git pull
-rm /var/node/wcubed-spa/src/environments/environment.ts
-mv /var/node/wcubed-spa/src/environments/environment.prod.ts /var/node/wcubed-spa/src/environments/environment.ts
 ng build -prod
