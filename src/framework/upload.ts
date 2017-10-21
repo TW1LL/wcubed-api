207b15313b7d5ae7e754531a8db0d9e40fd306d1
Tom Fitzpatrick <tfitz237@gmail.com>
2017-10-21 19:02:12 -0400

    fix: upload
    possibly

--- src/framework/upload.ts
+++ src/framework/upload.ts
@@ -10,17 +10,28 @@ export async function upload(ctx: Context, db: Connection) {
     const userAuth = db.getRepository(UserAuth);
     const [valid, user] = await new Auth(ctx, userAuth).authorized(rankTitle.Mod);
     if (valid) {
+
         const file = ctx.request.body.files.image;
         const type = ctx.params.type;
-        logger.log('UPLOAD >> ' + type);
-        const reader = fs.createReadStream(file.path);
-        const stream = fs.createWriteStream(path.join('/var/www/wcubed-spa/assets/images', type, file.name));
-        await reader.pipe(stream);
-        ctx.body = {
-            result: 'uploading'
-        }
+        uploadImage(file, type).then(() => {
+            ctx.body = {
+                result: 'uploading'
+            }
+        });
+
+
     } else {
         ctx.body = false;
     }
 
+}
+
+function uploadImage(file, type) {
+    return new Promise((resolve) => {
+        logger.log('UPLOAD >> ' + type);
+        const reader = fs.createReadStream(file.path);
+        const stream = fs.createWriteStream(path.join('/var/www/wcubed-spa/assets/images', type, file.name));
+        reader.pipe(stream);
+        resolve(true);
+    })
 }
