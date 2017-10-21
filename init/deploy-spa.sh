3280115f97b6ca33e3da39d800f12e8cdcb7ffdb
Will Wagner <willwagner602@gmail.com>
2017-10-21 17:58:33 -0400

    Updated with full build paths

--- init/deploy-spa.sh
+++ init/deploy-spa.sh
@@ -1,5 +1,5 @@
 cd /var/node/wcubed-spa
 git pull
-rm src/environment.ts
-mv src/environment.prod.ts /src/environment.ts
+rm /var/node/wcubed-spa/src/environment.ts
+mv /var/node/wcubed-spa/src/environment.prod.ts /var/node/wcubed-spa/src/environment.ts
 ng build -prod
