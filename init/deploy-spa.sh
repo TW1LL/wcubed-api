ce3dc243d50eb82ac23221bd44d0e64478b35206
Will Wagner <willwagner602@gmail.com>
2017-10-21 18:03:07 -0400

    wow I'm bad at this

--- init/deploy-spa.sh
+++ init/deploy-spa.sh
@@ -1,5 +1,5 @@
 cd /var/node/wcubed-spa
 git pull
-rm /var/node/wcubed-spa/src/environment.ts
-mv /var/node/wcubed-spa/src/environment.prod.ts /var/node/wcubed-spa/src/environment.ts
+rm /var/node/wcubed-spa/src/environments/environment.ts
+mv /var/node/wcubed-spa/src/environments/environment.prod.ts /var/node/wcubed-spa/src/environments/environment.ts
 ng build -prod
